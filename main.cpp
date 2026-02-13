565a33da5ff0501130e0b60a3a60e35415e1513a
UgniusPi <upilipavicius@gmail.com>
2026-02-13 11:31:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 using std::string;
 using std::vector;
 using std::cout;
@@ -19,21 +20,22 @@ struct Studentas {
     double gal;
 };
 
-void ivestis(vector<Studentas> &studentai);
-void isvestis(vector<Studentas> &studentai);
-void skaicGal(vector<Studentas> &studentai);
+void ivestis(vector<Studentas> &studentai, int &pasirink);
+void isvestis(vector<Studentas> &studentai, int pasirink);
+void skaicGal(vector<Studentas> &studentai, int pasirink);
 
 int main() {
     vector<Studentas> studentai;
+    int pasirink;
     
-    ivestis(studentai);
-    skaicGal(studentai);
-    isvestis(studentai);
+    ivestis(studentai, pasirink);
+    skaicGal(studentai, pasirink);
+    isvestis(studentai, pasirink);
     
     return 0;
 }
 
-void ivestis(vector<Studentas> &studentai) {
+void ivestis(vector<Studentas> &studentai, int &pasirink) {
     int studSk;
     
     cout << "Iveskite studentu skaiciu: ";
@@ -70,26 +72,59 @@ void ivestis(vector<Studentas> &studentai) {
         
         studentai.push_back(naujasStud);
     } 
+    
+    cout << "Pasirinkite galutinio rezultato skaiciavimo buda.\n";
+    cout << "Jeigu norite skaiciuoti naudojant vidurki, iveskite 1.\n";
+    cout << "Jeigu norite skaiciuoti naudojant mediana, iveskite 2.\n";
+    cout << "Pasirinkimas: ";
+    cin >> pasirink;
 }
 
-void isvestis(vector<Studentas> &studentai) {
-    cout << left << setw(20) << "Pavarde" << left << setw(15) << "Vardas" << left << setw(20) << "Galutinis (Vid.)" << '\n';
+void isvestis(vector<Studentas> &studentai, int pasirink) {
+    string galTekstas;
+    if (pasirink == 1) {
+        galTekstas = "Galutinis (Vid.)";
+    }
+    else {
+        galTekstas = "Galutinis (Med.)";
+    }
+    cout << left << setw(20) << "Pavarde" << left << setw(15) << "Vardas" << left << setw(20) << galTekstas << '\n';
     cout << "-------------------------------------------------------" << '\n';
     for (auto stud : studentai) {
         cout << left << setw(20) << stud.pav << left << setw(15) << stud.vard << left << setw(20) << fixed << setprecision(2) << stud.gal << '\n';
     }
 }
 
-void skaicGal(vector<Studentas> &studentai) {
-    for (auto &stud : studentai) {
-        int suma = 0;
-        double vid = 0;
-        
-        for (int paz : stud.tarp) {
-            suma += paz;
+void skaicGal(vector<Studentas> &studentai, int pasirink) {
+    if (pasirink == 1) {
+        for (auto &stud : studentai) {
+            int suma = 0;
+            double vid = 0;
+            
+            for (int paz : stud.tarp) {
+                suma += paz;
+            }
+            
+            vid = (double)suma / stud.tarp.size();
+            stud.gal = vid * 0.4 + (double)stud.egz * 0.6;   
         }
-        
-        vid = (double)suma / stud.tarp.size();
-        stud.gal = vid * 0.4 + (double)stud.egz * 0.6;   
+    }
+    else if (pasirink == 2) {
+        for (auto &stud : studentai) {
+            double med;
+            
+            sort(stud.tarp.begin(),stud.tarp.end());
+            
+            int medIndex;
+            medIndex = stud.tarp.size() / 2;
+            if (stud.tarp.size() % 2 == 1) {
+                med = stud.tarp[medIndex];
+            }
+            else {
+                med = (double)(stud.tarp[medIndex] + stud.tarp[medIndex - 1]) / 2;
+            }
+
+            stud.gal = med * 0.4 + (double)stud.egz * 0.6;   
+        }   
     }
 }
