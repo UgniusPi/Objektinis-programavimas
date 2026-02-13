97163045de2186b004a21019bf66433489d0120a
UgniusPi <upilipavicius@gmail.com>
2026-02-13 09:25:56 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using std::string;
+using std::vector;
+
+struct Studentas {
+  string vard;
+  string pav;
+  vector<int> tarp;
+  int egz;
+  double gal;
+};
+
+int main() {
+  return 0;
+}
