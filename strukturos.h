de1fa54a308643248f7040132cbffcf6b936816f
UgniusPi <upilipavicius@gmail.com>
2026-03-06 23:00:42 +0200

    update

---
+++ strukturos.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+struct Studentas {
+    string vard;
+    string pav;
+    vector<int> tarp;
+    int egz;
+    double gal;
+};
+
+struct Nustatymai {
+    int ivestSaltinis;
+    int isvestVieta;
+    int pasirink;
+    int rusBudas;
+    bool rndPaz;
+    bool rndVard;
+};
