24b7bf1daa661157fe3ca0523ca26f7d8c944793
UgniusPi <upilipavicius@gmail.com>
2026-03-13 22:02:08 +0200

    update

--- generatorius.cpp
+++ generatorius.cpp
@@ -11,19 +11,24 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    ofstream file("stud10000000.txt");
-    int studSk = 10000000;
+    ofstream file("stud100.txt");
+    int studSk = 100;
+    int pazSk = 12;
 
-    file << "Vardas                   Pavarde                    ND1       ND2       ND3       ND4       ND5       ND6       ND7       ND8       ND9      ND10      ND11      ND12      ND13      ND14      ND15      Egz.\n";
+    file << left << setw(16) << "Vardas" << right << setw(16) << "Pavarde" << setw(13) << " ";
+    for (int i=0; i<pazSk; i++) {
+        file << right << setw(10) << "ND" + to_string(i + 1);
+    }
+    file << right << setw(10) << "Egz." << "\n"; 
+
     for (int i=0; i<studSk; i++) {
         file << left << setw(16) << "Vardas" + to_string(i + 1) << right << setw(16) << "Pavarde" + to_string(i + 1);
-        file << setw(20) << " ";
-        for (int j=0; j<15; j++) {
-            file << right << setw(3) << to_string(rand() % 10 + 1) << setw(7) << " ";
+        file << setw(13) << " ";
+        for (int j=0; j<pazSk; j++) {
+            file << right << setw(10) << to_string(rand() % 10 + 1);
         }
-        file << right << setw(3) << to_string(rand() % 10 + 1) << "\n";
+        file << right << setw(10) << to_string(rand() % 10 + 1) << "\n";
     }
-
 
     return 0;
 }
